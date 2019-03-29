c383096fb7b67e0641318f144f8d2b251bfec948
Mingyyy <j.yanming@gmail.com>
2019-03-29 17:13:39 +0800

    add photo

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),)
+    os.path.join(BASE_DIR, 'logs/static'),)
 
 # Extra places for collectstatic to find static files.
 
