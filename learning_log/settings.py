b1c210e617a1de22fabc9cd10af4eb519674a04b
Mingyyy <j.yanming@gmail.com>
2019-03-29 16:02:36 +0800

    Ready to heroku this for the fifth time.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -84,24 +84,25 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-    }
-}
-# DATABASES={
+#
+# DATABASES = {
 #     'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ.get('learninglog', ''),
-#             'USER': os.environ.get('Ming',''),
-#             'PASSWORD':os.environ.get('',''),
-#             'HOST':'localhost',
-#             'PORT':'5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#
 #     }
 # }
+
+DATABASES={
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'learninglog',
+            'USER': 'Ming',
+            'PASSWORD':'',
+            'HOST':'localhost',
+            'PORT':'5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
