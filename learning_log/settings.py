56fc4ac5eb14de94735544cdbabd91f9849570cf
Mingyyy <j.yanming@gmail.com>
2019-03-29 15:58:26 +0800

    Ready to heroku this for the fifth time.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # Static asset configuration
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'logs/static'),)
