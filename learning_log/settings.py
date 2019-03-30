a5d06b2b8aa445f3987f27261103c1c1218eafc7
Mingyyy <j.yanming@gmail.com>
2019-03-30 14:15:26 +0800

    tyring to solve static file issue

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -155,6 +155,6 @@ STATICFILES_DIRS = (
 
 # https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/cerulean/bootstrap.css
 # https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css
-#BOOTSTRAP3 = {'theme_url': 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.7.7/litera/bootstrap.css',}
+BOOTSTRAP3 = {'theme_url': 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.1/yeti/bootstrap.css',}
 
 django_heroku.settings(locals())
